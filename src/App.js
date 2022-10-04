ccdda73f1028ad07730603c30396d4b53fb174e0
Hasan Kc <alhasanal_qaysi@yahoo.com>
2022-10-04 23:05:09 +0300

    Created the Social links page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './index.css';
 import Navbar from './compornts/NaveBar'
 import Home from './compornts/Home'
-
+import SocialLinks from './compornts/SocialLinks'
 
 function App() {
   return (
     <div >
       <Navbar />
       <Home />
+      <SocialLinks />
     </div>
   );
 }
